5586156993096c994a8d3cb3d0666a60e97d1a72
Zishan Panchal <36021217+zishpanchal@users.noreply.github.com>
2024-01-17 14:50:20 +0530

    Delete backend directory

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const mongoURI = 'mongodb://localhost:27017/inotebook'
-
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI).then(()=>console.log("Connected to mongo")).catch((e)=>console.log(e.message))
-}
-
-module.exports = connectToMongo;
